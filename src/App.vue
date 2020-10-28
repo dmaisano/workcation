223a2a665ab59c9002a9862b41b09bee7130057c
Domenico Maisao <dmaisanooo@gmail.com>
2020-10-28 18:18:51 -0400

    Removed old project; Created vue 3 preview project

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,25 @@
 <template>
-  <div id="app" class="min-h-screen bg-gray-200 antialiased">
-    <SiteHeader />
-
-    <SearchFilters />
-
-    <main class="px-4 py-6">
-      <h3 class="text-gray-900 text-xl">Los Angeles</h3>
-      <p class="text-gray-600">Live like the stars in these luxurious Southern California estates.</p>
-
-      <div class="mt-6">
-        <div>
-          <img
-            class="rounded-lg shadow-md h-64"
-            src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-            alt="modern mansion"
-          />
-        </div>
-        <div class="relative px-4 -mt-16">
-          <div class="bg-white rounded-lg shadow-lg px-4 py-3">
-            <div class="inline-flex">
-              <span
-                class="inline-block px-2 py-1 leading-none bg-teal-200 text-teal-800 rounded-full font-semibold uppercase tracking-wide text-xs"
-              >Plus</span>
-              <div
-                class="ml-2 text-xs text-gray-600 font-semibold uppercase tracking-wide"
-              >3 beds &bull; 2 baths</div>
-            </div>
-            <h4 class="text-gray-900 font-semibold text-lg">Modern home in city center</h4>
-            <div class="mt-1">
-              <span class="text-gray-900">$1,400</span>
-              <span class="ml-1 text-gray-600">/wk</span>
-            </div>
-            <div class="mt-1 text-sm text-gray-600 inline-flex items-center">
-              <svg
-                v-for="(value, index) in 5"
-                :key="index"
-                class="h-4 w-4 fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-                />
-              </svg>
-              <span class="ml-1">34 reviews</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+  <div id="app">
+    <h1>Workcation</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import SiteHeader from './components/SiteHeader.vue';
-import SearchFilters from './components/SearchFilters.vue';
+import { defineComponent } from "vue";
 
-@Component({
-  name: 'app',
-  props: [],
-  components: {
-    SiteHeader,
-    SearchFilters,
-  },
-})
-export default class App extends Vue {}
+export default defineComponent({
+  name: "App",
+  components: {},
+});
 </script>
 
-<style src="./assets/css/main.css"></style>
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
