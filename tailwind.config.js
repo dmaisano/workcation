871b815b85f27f323c7359b263264b0687f8fb78
Domenico Maisano <dmaisanooo@gmail.com>
2021-10-26 21:20:12 -0400

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  purge:
+    process.env.NODE_ENV === "production"
+      ? ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"]
+      : [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
